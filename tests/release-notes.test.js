b2d35ce815b1f61f32692456afc82de994f70974
Yash Anand <yashanand1910@gmail.com>
2024-04-09 13:12:35 -0400

    fix: failing tests for v[X.Y.Z] case

--- tests/release-notes.test.js
+++ tests/release-notes.test.js
@@ -1,5 +1,5 @@
 import { extractReleaseNotes, extractReleaseNotesFromPath } from "../src/release-notes";
-import {test} from '@jest/globals';
+import { test } from "@jest/globals";
 
 const changelogContent = `
 # Changelog
@@ -10,35 +10,27 @@ This changelog is only for testing purposes.
 
 notes for specific version 1
 
-### [0.0.2-beta.1](https://github.com/yashanand1910/rozgar.today/compare/v0.0.2-beta.0...v0.0.2-beta.1) (2020-10-01)
+### v[0.0.2-beta.1](https://github.com/yashanand1910/rozgar.today/compare/v0.0.2-beta.0...v0.0.2-beta.1) (2020-10-01)
 
 notes for specific version 2
-`
+`;
 
-test('extract release notes from string 1', async () => {
-
-  const notes = await extractReleaseNotes(changelogContent, '0.0.2-beta.2');
-  expect(notes).toBe('notes for specific version 1');
-
+test("extract release notes from string 1", async () => {
+  const notes = await extractReleaseNotes(changelogContent, "0.0.2-beta.2");
+  expect(notes).toBe("notes for specific version 1");
 });
 
-test('extract release notes from string 2', async () => {
-
-  const notes = await extractReleaseNotes(changelogContent, '0.0.2-beta.1');
-  expect(notes).toBe('notes for specific version 2');
-
+test("extract release notes from string 2", async () => {
+  const notes = await extractReleaseNotes(changelogContent, "0.0.2-beta.1");
+  expect(notes).toBe("notes for specific version 2");
 });
 
-test('extract release notes from path 1', async () => {
-
-  const notes = await extractReleaseNotesFromPath('tests/test-changelog.md', '0.0.2-beta.2');
-  expect(notes).toBe('notes for specific version 1');
-
+test("extract release notes from path 1", async () => {
+  const notes = await extractReleaseNotesFromPath("tests/test-changelog.md", "0.0.2-beta.2");
+  expect(notes).toBe("notes for specific version 1");
 });
 
-test('extract release notes from path 2', async () => {
-
-  const notes = await extractReleaseNotesFromPath('tests/test-changelog.md', 'refs/tags/0.0.2-beta.1');
-  expect(notes).toBe('notes for specific version 2');
-
+test("extract release notes from path 2", async () => {
+  const notes = await extractReleaseNotesFromPath("tests/test-changelog.md", "refs/tags/0.0.2-beta.1");
+  expect(notes).toBe("notes for specific version 2");
 });
